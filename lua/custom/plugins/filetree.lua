17162111e41690fe1f9a8e261d9e89ad3fc203c8
Carl Gützkow <cjgutzkow@gmail.com>
2023-11-22 18:42:30 +0100

    show current file in filetree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -13,9 +13,14 @@ return {
   config = function()
     require('neo-tree').setup {
       close_if_last_window = true,
+      follow_current_file = {
+        enabled = true,          -- This will find and focus the file in the active buffer every time
+        --               -- the current file is changed while the tree is open.
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
     }
 
-    vim.keymap.set("n", "<leader>f", ":Neotree<CR>",
+    vim.keymap.set("n", "<leader>f", ":Neotree filesystem reveal<CR>",
       { desc = 'Open Neotree [f]older structure. Open tree or go to selected window' })
   end,
 }
