533ac1cf618cba1c68149b4c0cfbb51adbb4f235
Carl Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-02-06 16:48:31 +0100

    change to gitsigns config

---
+++ config/nvim/lua/custom/plugins/gitsigns.lua
@@ -0,0 +1,19 @@
+return {
+  "https://github.com/lewis6991/gitsigns.nvim",
+  config = function()
+    require("gitsigns").setup {
+      linehl = false,
+      watch_index = { interval = 1000 },
+      sign_priority = 6,
+      status_formatter = nil,
+      current_line_blame = true,
+    }
+
+    vim.keymap.set("n", "<leader>gh", ":Gitsigns next_hunk<CR>", {
+      desc = "next git [h]unk",
+    })
+    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {
+      desc = "[p]review current git hunk",
+    })
+  end
+}
