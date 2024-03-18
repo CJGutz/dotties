1ee63a00378c1b4c662d9dbcdab50bbcf35476fd
Carl Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-03-18 15:44:16 +0100

    unpatch colored message

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -74,10 +74,10 @@ require('lazy').setup({
 
   {
     -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'folke/tokyonight.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'tokyonight-storm'
     end,
   },
 
@@ -88,7 +88,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = true,
-        theme = 'onedark',
+        theme = 'tokyonight',
         component_separators = '|',
         section_separators = '',
       },
